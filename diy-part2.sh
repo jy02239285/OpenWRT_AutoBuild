34dabc52889d923593182a8f5bcd32ceaec76b2f
jy02239285 <53409939+jy02239285@users.noreply.github.com>
2025-09-09 22:03:34 -0400

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,8 +8,8 @@
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/OpenWrt.Lan/g' package/base-files/files/bin/config_generate
 
 # Modify default IP 10.0.0.1/24
-sed -i "/ipaddr/s/.*/\t\tset network.lan.ipaddr='10.0.0.1'/" package/base-files/files/bin/config_generate
-sed -i "/netmask/s/.*/\t\tset network.lan.netmask='255.255.255.0'/" package/base-files/files/bin/config_generate
+#sed -i "/ipaddr/s/.*/\t\tset network.lan.ipaddr='10.0.0.1'/" package/base-files/files/bin/config_generate
+#sed -i "/netmask/s/.*/\t\tset network.lan.netmask='255.255.255.0'/" package/base-files/files/bin/config_generate
